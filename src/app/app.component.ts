2a8f6c9ab64439e9cadebdb0453153e8d34520bf
amelsadoun <amelsadoun.b@gmail.com>
2024-12-08 11:38:51 +0100

    initialized project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: '<div>slm lebess??</div>',
+})
+export class AppComponent {
+  title = 'projet-igl';
+}
