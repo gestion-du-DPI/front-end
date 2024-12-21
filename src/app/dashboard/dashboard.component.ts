ea65125b74a0359d5a962413f1c515da6798379a
Zineb <mz_atir@esi.dz>
2024-12-21 21:30:07 +0100

    added function to calculate the cards statistics

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../components/header/header.component';
 import { StatisticsGraphComponent } from '../components/statistics-graph/statistics-graph.component';
+import { CardComponent } from '../components/dashboard-card/dashboard-card.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, HeaderComponent, StatisticsGraphComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    StatisticsGraphComponent,
+    CardComponent,
+  ],
   template: `
     <div class="flex flex-col">
       <div class="flex flex-col gap-4 lg:mx-16 mx-3">
@@ -31,222 +37,180 @@ import { StatisticsGraphComponent } from '../components/statistics-graph/statist
 
       <!-- DASHBOARD OVERVIEW -->
       <div
-        class=" flex justify-start pt-4 gap-9"
+        class=" flex justify-start pt-4 gap-7"
         style="margin-left: 95px; margin-right: 50px;"
       >
-        <!-- Dashboard Card 1 -->
+        <app-card
+          [title]="'Total Patients'"
+          [count]="totalPatients"
+          [percentage]="calculatePercentage(totalPatients, previousPatients)"
+          [color]="'bg-main'"
+          [icon]="'dashboard-patient.svg'"
+        ></app-card>
+
+        <app-card
+          [title]="'Total Doctors'"
+          [count]="totalDoctors"
+          [percentage]="calculatePercentage(totalDoctors, previousDoctors)"
+          [color]="'bg-secondary'"
+          [icon]="'dashboard-doctor.svg'"
+        ></app-card>
+
+        <app-card
+          [title]="'Total Nurses'"
+          [count]="totalNurses"
+          [percentage]="calculatePercentage(totalNurses, previousNurses)"
+          [color]="'bg-tertiary'"
+          [icon]="'dashboard-nurse.svg'"
+        ></app-card>
+
+        <app-card
+          [title]="'Lab/Radiologists'"
+          [count]="totalRadiologists"
+          [percentage]="calculatePercentage(totalRadiologists, previousRadiologists)"
+          [color]="'bg-quaternary'"
+          [icon]="'dashboard-add.svg'"
+        ></app-card>
+
+        <app-card
+          [title]="'Total Consultations'"
+          [count]="totalConsultations"
+          [percentage]="calculatePercentage(totalConsultations, previousConsultations)"
+          [color]="'bg-fifth'"
+          [icon]="'dashboard-cons.svg'"
+        ></app-card>
+      </div>
+
+    <!-- DASHBORAD CONTENT -->
+    <div class="flex justify-between items-center pt-8 w-[1310px] gap-9">
+      <!-- STATISTICS AND RECENT PATIENTS -->
+      <div
+        class="w-[900px] h-[704px] gap-7"
+        style="margin-left: 50px;margin-right:0px; padding-right:0px; "
+      >
+        <!-- STATISTICS -->
         <div
-          class="w-[214px] h-[110px] bg-[#999BFD] rounded-lg p-4 border-2 border-[#F0F0F0] flex flex-col gap-2"
+          class="bg-white w-[900px] h-[338px] border-2 border-[#F4F2F2] rounded-[16px] pt-6 pl-6 pb-6 pr-12 m-0"
+          style="margin-bottom: 30px;"
         >
-          <div class="flex justify-between items-center">
-            <p class="font-semibold text-sm opacity-60 font-plus-jakarta">
-              Total Patients
+          <div class="flex justify-between items-center pr-4 pb-4">
+            <p class="text-main" style="font-weight: 600; font-size:20px ">
+              Statistics
             </p>
-            <img src="dashboard-patient.svg" alt="patientImage" />
+            <img src="dashboard-stat.svg" alt="" />
           </div>
-          <div class="flex justify-between items-center">
-            <p class="text-3xl font-bold font-plus-jakarta   ">911</p>
-            <p class="text-sm font-light opacity-60 font-plus-jakarta">+25%</p>
+          <div>
+            <app-statistics-graph></app-statistics-graph>
           </div>
         </div>
-
-        <!-- Dashboard Card 2 -->
+        <!-- RECENT PATIENTS -->
         <div
-          class="w-[214px] h-[110px] bg-[#A7DAF8] rounded-lg p-4 border-2 border-[#F0F0F0] flex flex-col gap-2"
+          class="bg-white w-[900px] h-[338px] border-2 border-[#F4F2F2] rounded-[16px] pt-6 pl-6 pb-6 pr-12"
         >
-          <div class="flex justify-between items-center">
-            <p class="font-semibold text-sm opacity-60 font-plus-jakarta">
-              Total Doctors
+          <div class="flex justify-between items-center pr-7">
+            <p class="text-main" style="font-weight: 600; font-size:20px">
+              Recent Patients
             </p>
-            <img src="dashboard-doctor.svg" alt="patientImage" />
+            <div class="mt-4 text-right flex  gap-5">
+              <a
+                href="#"
+                class="text-[#667085] font-bold text-[10px] cursor-pointer hover:underline font-plus-jakarta"
+                style="letter-spacing: 1px; "
+                >SEE ALL PATIENTS</a
+              >
+              <img src="dashboard-seeall.svg" alt="" />
+            </div>
           </div>
-          <div class="flex justify-between items-center">
-            <p class="text-3xl font-bold font-plus-jakarta">911</p>
-            <p class="text-sm font-light opacity-60 font-plus-jakarta">+25%</p>
-          </div>
-        </div>
-
-        <!-- Dashboard Card 3 -->
-        <div
-          class="w-[214px] h-[110px] bg-[#8FDBF2] rounded-lg p-4 border-2 border-[#F0F0F0] flex flex-col gap-2"
-        >
-          <div class="flex justify-between items-center">
-            <p class="font-semibold text-sm opacity-60 font-plus-jakarta">
-              Total Nurses
-            </p>
-            <img src="dashboard-nurse.svg" alt="patientImage" />
-          </div>
-          <div class="flex justify-between items-center">
-            <p class="text-3xl font-bold font-plus-jakarta">911</p>
-            <p class="text-sm font-light opacity-60 font-plus-jakarta">+25%</p>
-          </div>
-        </div>
-
-        <!-- Dashboard Card 4 -->
-        <div
-          class="w-[214px] h-[110px] bg-[#92E2EE] rounded-lg p-4 border-2 border-[#F0F0F0] flex flex-col gap-2"
-        >
-          <div class="flex justify-between items-center">
-            <p class="font-semibold text-sm opacity-60 font-plus-jakarta">
-              lab/radiologists
-            </p>
-            <img src="dashboard-add.svg" alt="patientImage" />
-          </div>
-          <div class="flex justify-between items-center">
-            <p class="text-3xl font-bold font-plus-jakarta">911</p>
-            <p class="text-sm font-light opacity-60 font-plus-jakarta">+25%</p>
-          </div>
-        </div>
-
-        <!-- Dashboard Card 5 -->
-        <div
-          class="w-[214px] h-[110px] bg-[#94E9E9] rounded-lg p-4 border-2 border-[#F0F0F0] flex flex-col gap-2"
-        >
-          <div class="flex justify-between items-center">
-            <p class="font-semibold text-sm opacity-60 font-plus-jakarta">
-              Total Consultations
-            </p>
-            <img src="dashboard-cons.svg" alt="patientImage" />
-          </div>
-          <div class="flex justify-between items-center">
-            <p class="text-3xl font-bold font-plus-jakarta">911</p>
-            <p class="text-sm font-light opacity-60 font-plus-jakarta">+25%</p>
+          <!---- RECENT PATIENTS TABLE -->
+          <div class="overflow-x-auto h-full">
+            <table class="min-w-full table-auto mt-6">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email Address</th>
+                  <th>Phone Number</th>
+                  <th>Social Number</th>
+                  <th>Address</th>
+                  <th>E^Contact</th>
+                  <th>E^Phone</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody
+                class=" bg-white rounded-lg overflow-hidden h-[calc(100%-40px)]"
+              >
+                <tr
+                  *ngFor="let patient of recentPatients"
+                  class="hover:bg-slate-50"
+                >
+                  <td class="flex flex-row gap-4 items-center">
+                    <img
+                      [src]="patient.profilePicture"
+                      class="w-8 h-8 rounded-full cursor-pointer"
+                      alt="Profile Picture"
+                    />
+                    <div class="flex flex-col">
+                      <span class="font-semibold text-[12px] w-[8] text-left">{{
+                        patient.name
+                      }}</span>
+                    </div>
+                  </td>
+                  <td>{{ patient.email }}</td>
+                  <td>{{ patient.phone }}</td>
+                  <td>{{ patient.socialNumber }}</td>
+                  <td>{{ patient.address }}</td>
+                  <td>{{ patient.emergencyContact }}</td>
+                  <td>{{ patient.emergencyPhone }}</td>
+                  <td>
+                    <img
+                      [src]="patient.qrCode"
+                      alt="QR Code"
+                      class="w-10 h-10 pl-4"
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
 
-      <!-- DASHBORAD CONTENT -->
-      <div class="flex justify-between items-center pt-8 w-[1310px] gap-9">
-        <!-- STATISTICS AND RECENT PATIENTS -->
-        <div
-          class="w-[900px] h-[704px] gap-7"
-          style="margin-left: 50px;margin-right:0px; padding-right:0px; "
-        >
-          <!-- STATISTICS -->
-          <div
-            class="bg-white w-[900px] h-[338px] border-2 border-[#F4F2F2] rounded-[16px] pt-6 pl-6 pb-6 pr-12 m-0"
-            style="margin-bottom: 30px;"
-          >
-            <div class="flex justify-between items-center pr-4 pb-4">
-              <p class="text-main" style="font-weight: 600; font-size:20px ">
-                Statistics
-              </p>
-              <img src="dashboard-stat.svg" alt="" />
-            </div>
-            <div>
-            <app-statistics-graph></app-statistics-graph>
-            </div>
-          </div>
-          <!-- RECENT PATIENTS -->
-          <div
-            class="bg-white w-[900px] h-[338px] border-2 border-[#F4F2F2] rounded-[16px] pt-6 pl-6 pb-6 pr-12"
-          >
-            <div class="flex justify-between items-center pr-7">
-              <p class="text-main" style="font-weight: 600; font-size:20px">
-                Recent Patients
-              </p>
-              <div class="mt-4 text-right flex  gap-5">
-                <a
-                  href="#"
-                  class="text-[#667085] font-bold text-[10px] cursor-pointer hover:underline font-plus-jakarta"
-                  style="letter-spacing: 1px; "
-                  >SEE ALL PATIENTS</a
-                >
-                <img src="dashboard-seeall.svg" alt="" />
+      <!-- Top Medical Staff -->
+      <div
+        class="w-full h-[704px] bg-white border-2 border-[#F4F2F2] rounded-[16px] p-6 flex flex-col"
+      >
+        <h3 class="text-[20px] font-semibold text-main mb-4">
+          Top Medical Staff
+        </h3>
+
+        <ul class="space-y-6 overflow-y-auto max-h-[1000px] flex-grow">
+          <!-- Loop through the top medical staff data -->
+          <li *ngFor="let staff of topMedicalStaff" class="flex flex-col ">
+            <div class="w-[210px] h-[47px] flex justify-between ">
+              <img
+                [src]="staff.profilePicture"
+                alt="Doctor Avatar"
+                class="w-10 h-10 rounded-full "
+              />
+              <div class="w-[210px] h-[47px] pl-4">
+                <p class="font-bold text-black text-[13px] text-left">
+                  {{ staff.name }}
+                </p>
+                <p class="font-normal	text-[#667085] text-[13px] text-left">
+                  {{ staff.role }}
+                </p>
               </div>
             </div>
-            <!---- RECENT PATIENTS TABLE -->
-            <div class="overflow-x-auto h-full">
-              <table class="min-w-full table-auto mt-6">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Email Address</th>
-                    <th>Phone Number</th>
-                    <th>Social Number</th>
-                    <th>Address</th>
-                    <th>E^Contact</th>
-                    <th>E^Phone</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody
-                  class=" bg-white rounded-lg overflow-hidden h-[calc(100%-40px)]"
-                >
-                  <tr
-                    *ngFor="let patient of recentPatients"
-                    class="hover:bg-slate-50"
-                  >
-                    <td class="flex flex-row gap-4 items-center">
-                      <img
-                        [src]="patient.profilePicture"
-                        class="w-8 h-8 rounded-full cursor-pointer"
-                        alt="Profile Picture"
-                      />
-                      <div class="flex flex-col">
-                        <span
-                          class="font-semibold text-[12px] w-[8] text-left"
-                          >{{ patient.name }}</span
-                        >
-                      </div>
-                    </td>
-                    <td>{{ patient.email }}</td>
-                    <td>{{ patient.phone }}</td>
-                    <td>{{ patient.socialNumber }}</td>
-                    <td>{{ patient.address }}</td>
-                    <td>{{ patient.emergencyContact }}</td>
-                    <td>{{ patient.emergencyPhone }}</td>
-                    <td>
-                      <img
-                        [src]="patient.qrCode"
-                        alt="QR Code"
-                        class="w-10 h-10 pl-4"
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-
-        <!-- Top Medical Staff -->
-        <div
-          class="w-full h-[704px] bg-white border-2 border-[#F4F2F2] rounded-[16px] p-6 flex flex-col"
-        >
-          <h3 class="text-[20px] font-semibold text-main mb-4">
-            Top Medical Staff
-          </h3>
-
-          <ul class="space-y-6 overflow-y-auto max-h-[1000px] flex-grow">
-            <!-- Loop through the top medical staff data -->
-            <li *ngFor="let staff of topMedicalStaff" class="flex flex-col ">
-              <div class="w-[210px] h-[47px] flex justify-between ">
-                <img
-                  [src]="staff.profilePicture"
-                  alt="Doctor Avatar"
-                  class="w-10 h-10 rounded-full "
-                />
-                <div class="w-[210px] h-[47px] pl-4">
-                  <p class="font-bold text-black text-[13px] text-left">
-                    {{ staff.name }}
-                  </p>
-                  <p class="font-normal	text-[#667085] text-[13px] text-left">
-                    {{ staff.role }}
-                  </p>
-                </div>
-              </div>
-            </li>
-          </ul>
-          <div class="mt-4 text-left flex   gap-5">
-            <a
-              href="#"
-              class="text-[#667085] font-bold text-[10px] cursor-pointer hover:underline font-plus-jakarta"
-              style="letter-spacing: 1px; "
-              >SEE ALL WORKERS</a
-            >
-            <img src="dashboard-seeall.svg" alt="" />
-          </div>
+          </li>
+        </ul>
+        <div class="mt-4 text-left flex   gap-5">
+          <a
+            href="#"
+            class="text-[#667085] font-bold text-[10px] cursor-pointer hover:underline font-plus-jakarta"
+            style="letter-spacing: 1px; "
+            >SEE ALL WORKERS</a
+          >
+          <img src="dashboard-seeall.svg" alt="" />
         </div>
       </div>
     </div>
@@ -378,4 +342,26 @@ export class DashboardComponent {
       profilePicture: 'doctor-avatar.svg',
     },
   ];
+
+// Current data
+totalPatients: number = 911;
+totalDoctors: number = 200;
+totalNurses: number = 150;
+totalRadiologists: number = 30;
+totalConsultations: number = 5000;
+
+// Previous data for percentage calculations
+previousPatients: number = 890;
+previousDoctors: number = 190;
+previousNurses: number = 140;
+previousRadiologists: number = 25;
+previousConsultations: number = 4800;
+
+// Function to calculate percentage change
+calculatePercentage(current: number, previous: number): string {
+  if (previous === 0) return '+0%'; // Avoid division by zero
+  const percentageChange = ((current - previous) / previous) * 100;
+  const formattedChange = percentageChange.toFixed(2);
+  return percentageChange >= 0 ? `+${formattedChange}%` : `${formattedChange}%`;
+}
 }
