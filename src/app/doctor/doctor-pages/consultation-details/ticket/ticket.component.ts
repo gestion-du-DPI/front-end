021e0ddb5d1c5d24f9d2a1445df4c198701f53bb
amelsadoun <amelsadoun.b@gmail.com>
2025-01-02 00:31:16 +0100

    added open ticket component

--- src/app/doctor/doctor-pages/consultation-details/ticket/ticket.component.ts
+++ src/app/doctor/doctor-pages/consultation-details/ticket/ticket.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { OpenTicketComponent } from "../../../doctor-components/open-ticket/open-ticket.component";
 
 @Component({
   selector: 'app-ticket',
   imports: [],
   template: `
-    <p>
-      ticket works!
-    </p>
+   <div>ticket works!</div> 
   `,
   styles: ``
 })
