87cb21834e74859d64944801e121074a4a527bdd
amelsadoun <amelsadoun.b@gmail.com>
2025-01-02 13:19:11 +0100

    fixed up doctor actions placement

--- src/app/doctor/doctor-pages/consultation-details/consultation-details.component.ts
+++ src/app/doctor/doctor-pages/consultation-details/consultation-details.component.ts
@@ -1,47 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { consultations } from '../../../mock-data/consultations';
 
 @Component({
   selector: 'app-consultation-details',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet],
   template: `
-    <div
-      class="absolute top-20 right-5 bg-white rounded-xl border-2 border-slate-100 flex flex-col w-80 gap-3 py-4"
-    >
-      <h2 class="text-main font-semibold text-xl ml-6 text-semibold">
-        Doctor actions
-      </h2>
-      <button
-        class="flex hover:text-main flex-row items-center gap-2 group w-full hover:bg-[#ECF1FF] px-4 py-2 font-semibold"
-        routerLink="/doctor/consultation-details/:id/ticket"
-
-      >
-        <div
-          class="flex justify-center group-hover:border-[#BBBCFF] w-10 h-10 align-middle items-center rounded-xl border-2 border-slate-200 p-2"
-        >
-          <img src="ticket-icon.svg" class="w-5 h-5 object-cover" alt="" />
-        </div>
-        Open a ticket
-      </button>
-      <button
-        class="flex hover:text-main flex-row items-center gap-2 group w-full hover:bg-[#ECF1FF] px-4 py-2 font-semibold"
-        routerLink="/doctor/consultation-details/:id/prescription"
-
-      >
-        <div
-          class="flex justify-center group-hover:border-[#BBBCFF] w-10 h-10 align-middle items-center rounded-xl border-2 border-slate-200 p-2"
-        >
-          <img src="prescription-icon.svg" class="w-4 h-4 object-cover" alt="" />
-        </div>
-        Prescription
-      </button>
-      <div class="flex flex-row gap-3 justify-center items-center m-3 text-sm">
-    <button class="w-full rounded-lg text-main bg-second font-semibold text-center py-2">Archive</button>    
-    <button class="w-full rounded-lg text-white bg-main font-semibold text-center py-2 ">Close</button>    
+ <div class="flex flex-row">
+      <h4 class="text-main text-3xl font-semibold m-5">Consultation Details</h4>
     </div>
-    </div>
-
     <router-outlet></router-outlet>
   `,
   styles: ``,
--- src/app/doctor/doctor-pages/consultation-details/prescription/prescription.component.ts
+++ src/app/doctor/doctor-pages/consultation-details/prescription/prescription.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
+import { DoctorActionsConsultationComponent } from '../../../doctor-components/doctor-actions-consultation/doctor-actions-consultation.component';
 
 @Component({
   selector: 'app-prescription',
-  imports: [],
+  imports: [DoctorActionsConsultationComponent],
   template: `
-    <p>
-      prescription works!
-    </p>
+    <div class="flex flex-col gap-5 p-5">
+      <div class="flex flex-row">
+        <!-- space for info -->
+        <app-doctor-actions-consultation
+          class="ml-auto"
+        ></app-doctor-actions-consultation>
+      </div>
+      <div class="flex flex-row">
+        <!-- space for attachements -->
+      </div>
+    </div>
   `,
-  styles: ``
+  styles: ``,
 })
-export class PrescriptionComponent {
-
-}
+export class PrescriptionComponent {}
